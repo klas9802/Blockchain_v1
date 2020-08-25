0f52cbae7d6e4b363b84b03772f4d9b165572cd4
MooncatMaster <klas9806@gmail.com>
2020-08-25 14:45:06 +0800

    加入專案檔案。

---
+++ Blockchain_v1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Blockchain_v1.Areas.Identity.Data;
+using Blockchain_v1.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Blockchain_v1.Areas.Identity.IdentityHostingStartup))]
+namespace Blockchain_v1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                    {
+                        options.SignIn.RequireConfirmedAccount = false;
+                        options.Password.RequireLowercase = false;
+                        options.Password.RequireUppercase = false;
+                    })
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
